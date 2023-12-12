7834f3e9f9325c99f6cd141d1bca5484bb860b4a
Domka28 <119631616+Domka28@users.noreply.github.com>
2023-12-12 16:13:11 +0100

    Update paths

--- Film.js
+++ Film.js
@@ -14,13 +14,13 @@ class Film {
             <div class="description">
                 <div class="title-star-rate">
                     <h3 class="title">${Title}</h3>
-                    <img class="star" src="images/icon.png">
+                    <img class="star" src="./images/Icon.png">
                     <p class="rate">${imdbRating}</p>
                 </div>
                 <div class="time-genre-watchlist">
                     <p>${Runtime}</p>
                     <p class="genre">${Genre}</p>
-                    <img class="icon" data-add='${Title}' id="icon" src="images/icon2.png">
+                    <img class="icon" data-add='${Title}' id="icon" src="./images/Icon2.png">
                     <p>Watchlist</p>
                 </div>
                 <div class="desc">
@@ -39,13 +39,13 @@ class Film {
         <div class="description">
             <div class="title-star-rate">
                 <h3 class="title">${Title}</h3>
-                <img class="star" src="images/icon.png">
+                <img class="star" src="./images/Icon.png">
                 <p class="rate">${imdbRating}</p>
             </div>
             <div class="time-genre-watchlist">
                 <p>${Runtime}</p>
                 <p class="genre">${Genre}</p>
-                <img class="icon" data-remove='${Title}' src="images/remowe_icon.png">
+                <img class="icon" data-remove='${Title}' src="./images/remove_icon.png">
                 <p>Remove</p>
             </div>
             <div class="desc">
--- watchlist.js
+++ watchlist.js
@@ -3,7 +3,6 @@ import Film from "./Film.js"
 const watchlistContent = document.getElementById("watchlist-content")
 
 function render() {
-    console.log("Str" + localStorage.getItem('Movies') + "end")
     let moviesArray = [];
     if (localStorage.getItem('Movies') != null) {
         moviesArray = JSON.parse(localStorage.getItem('Movies'))
@@ -14,19 +13,17 @@ function render() {
         watchlistContent.innerHTML = `<div class="empty-watchlist">
         <h1 class="empty-text">Your watchlist is looking a little empty...</h1>
         <div class="add-movies">
-        <a href="/index.html"> <img class="add-icon" src="images/Icon2.png"></a>
+        <a href="/index.html"> <img class="add-icon" src="./images/Icon2.png"></a>
             <p class="add-text">Let’s add some movies!</p>
         </div>
     </div>`
     } else {
         watchlistContent.innerHTML = ""
         for (let movie of moviesArray) {
-            console.log(movie)
             fetch(`https://www.omdbapi.com/?apikey=a2742fc0&t=${movie}`)
                 .then(resp => resp.json())
                 .then(data => {
                     let newFilm = new Film(data)
-                    console.log(moviesArray.length)
                     watchlistContent.innerHTML += newFilm.getWatchlistHtml()
                 })
         }
@@ -38,7 +35,6 @@ document.addEventListener("click", function (e) {
     if (e.target.dataset.remove) {
         const moviesArray = JSON.parse(localStorage.getItem('Movies'))
         const index = moviesArray.indexOf(e.target.dataset.remove);
-        console.log(index)
         if (index > -1) {
             moviesArray.splice(index, 1);
             localStorage.setItem("Movies", JSON.stringify(moviesArray))
